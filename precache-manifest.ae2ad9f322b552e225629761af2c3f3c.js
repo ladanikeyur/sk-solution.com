c35c0b3f6da27995f0130cefc1ec72654c427b66
ladanikeyur <72188755+ladanikeyur@users.noreply.github.com>
2020-10-13 17:29:31 +0530

    Updates

---
+++ precache-manifest.ae2ad9f322b552e225629761af2c3f3c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db40c495199ef58e50a5d15010f94fe",
+    "url": "/sk-solution.com/index.html"
+  },
+  {
+    "revision": "bf9fe6bda1f527b0d4c8",
+    "url": "/sk-solution.com/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f84a2c6e1c8a9d448020",
+    "url": "/sk-solution.com/static/css/main.a68f3770.chunk.css"
+  },
+  {
+    "revision": "bf9fe6bda1f527b0d4c8",
+    "url": "/sk-solution.com/static/js/2.ed1244e2.chunk.js"
+  },
+  {
+    "revision": "dee1d41698e117cf46b74dc1c8e86424",
+    "url": "/sk-solution.com/static/js/2.ed1244e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f84a2c6e1c8a9d448020",
+    "url": "/sk-solution.com/static/js/main.11836710.chunk.js"
+  },
+  {
+    "revision": "846781c8ff744361f7e1",
+    "url": "/sk-solution.com/static/js/runtime-main.5d1886e3.js"
+  },
+  {
+    "revision": "0e824c399edb68aa68b5d2394af6693a",
+    "url": "/sk-solution.com/static/media/aboutus.0e824c39.png"
+  },
+  {
+    "revision": "8af9a6cb24dcf4b9fb647c6f1e5b815c",
+    "url": "/sk-solution.com/static/media/home1.8af9a6cb.gif"
+  }
+]);
